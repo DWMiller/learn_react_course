5f1360f91840939e998dd652b7c4069c846d9c81
David Miller <davidabc66@gmail.com>
2017-08-15 16:48:10 -0400

    8, stateless component, props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Header extends Component {
-  render() {
-    return <div>Header</div>;
-  }
-}
+const Header = props => {
+  return (
+    <header className="top">
+      <h1>
+        Catch
+        <span className="ofThe">
+          <span className="of">of</span>
+          <span className="the">the</span>
+        </span>
+        Day
+      </h1>
+      <h3 className="tagline">
+        <span>
+          {props.tagline}
+        </span>
+      </h3>
+    </header>
+  );
+};
+
+export default Header;
