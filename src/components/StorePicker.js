66c521d3a1bbdca82162680d6ea9fdc7cffca8e0
David Miller <davidabc66@gmail.com>
2017-08-15 20:18:12 -0400

    12, store select, handle event to route

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export default class StorePicker extends Component {
+import { getFunName } from '../helpers';
+
+class StorePicker extends Component {
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  }
+
+  goToStore(event) {
+    event.preventDefault();
+    this.context.router.transitionTo(`/store/${this.storeInput.value}`);
+  }
+
   render() {
     return (
-      <form className="">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={input => {
+            this.storeInput = input;
+          }}
+        />
         <button type="submit">Visit Store</button>
       </form>
     );
   }
 }
+
+StorePicker.contextTypes = {
+  router: PropTypes.object,
+};
+
+export default StorePicker;
