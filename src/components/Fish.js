1f3c568ece798e189d8ca0fc23b2f07b6e733f82
David Miller <davidabc66@gmail.com>
2018-03-04 21:19:01 -0500

    converted to stateless function component

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,46 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { formatPrice } from '../helpers';
 
-export class Fish extends Component {
-  render() {
-    const { name, price, desc, image, status, index } = this.props;
+const Fish = props => {
+  const { name, price, desc, image, status, index } = props;
 
-    const isAvailable = status === 'available';
-    const buttonText = isAvailable ? 'Add To Order' : 'Sold Out!';
+  const isAvailable = status === 'available';
+  const buttonText = isAvailable ? 'Add To Order' : 'Sold Out!';
 
-    return (
-      <li className="menu-fish">
-        <img src={image} alt={name} />
-        <h3 className="fish-name">
-          {name}
-          <span className="price">
-            {formatPrice(price)}
-          </span>
-        </h3>
-        <p>
-          {desc}
-        </p>
-        <button
-          disabled={!isAvailable}
-          onClick={() => this.props.addToOrder(index)}
-        >
-          {buttonText}
-        </button>
-      </li>
-    );
-  }
+  return (
+    <li className="menu-fish">
+      <img src={image} alt={name} />
+      <h3 className="fish-name">
+        {name}
+        <span className="price">{formatPrice(price)}</span>
+      </h3>
+      <p>{desc}</p>
+      <button disabled={!isAvailable} onClick={() => props.addToOrder(index)}>
+        {buttonText}
+      </button>
+    </li>
+  );
+};
 
-  static propTypes = {
-    addToOrder: PropTypes.func.isRequired,
-    name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    desc: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
-    index: PropTypes.string.isRequired,
-  };
-}
+Fish.propTypes = {
+  addToOrder: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  desc: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  index: PropTypes.string.isRequired,
+};
 
 export default Fish;
